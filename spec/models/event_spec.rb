5a9963514c5069119f5e430830f953b0c375ebf0
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-07-05 14:02:38 +0200

    Added scope in_week + tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe Event, type: :model do
       expect(event).to be_valid
     end
   end
+
+  describe '#in_week' do
+    let(:start_time) { Time.now.beginning_of_week }
+    let!(:events) {
+      [
+          FactoryGirl.create(:event, start_time: start_time, end_time: start_time + 2.hours),
+          FactoryGirl.create(:event, start_time: start_time + 1.days, end_time: start_time + 1.days + 2.hours),
+          FactoryGirl.create(:event, start_time: start_time - 2.days, end_time: start_time - 2.days + 1.hours),
+          FactoryGirl.create(:event, start_time: start_time - 2.days, end_time: start_time + 2.days),
+          FactoryGirl.create(:event, start_time: start_time - 2.days, end_time: start_time + 10.days),
+          FactoryGirl.create(:event, start_time: start_time + 1.days, end_time: start_time + 10.days)
+
+      ]
+    }
+
+    it "returns all events from specified week" do
+      expected_events = events.clone.tap { |n| n.delete_at(2) }
+      expect(Event.in_week(start_time).sort).to eq expected_events.sort
+    end
+
+  end
 end
