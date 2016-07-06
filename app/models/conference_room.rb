be9f4996dde4e0046162c5851bd0d3475473362d
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-07-06 16:47:26 +0200

    ConferenceRoom has many Events

--- app/models/conference_room.rb
+++ app/models/conference_room.rb
@@ -6,4 +6,6 @@ class ConferenceRoom < ApplicationRecord
   validates :color, presence: true, uniqueness: { case_sensitive: false }, format: %r{\A#[0-9a-f]{3}([0-9a-f]{3})?\z}i
   validates :title, presence: true, uniqueness: true
 
+  has_many :events, dependent: :destroy
+
 end
