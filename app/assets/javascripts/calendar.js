3f3bf75654f8c5b9aa97c329d0d6e333e20996de
Beata Obrok <beata.obrok@u2i.com>
2016-07-07 11:47:59 +0200

    Improve datepicker autocompletion

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -14,14 +14,14 @@ $(function () {
     var endData = endTimePicker.data('DateTimePicker');
 
     startTimePicker.on('dp.change', function (e) {
-        endData.minDate(e.date);
-        if (!endData.date()) {
+        var endDate = endData.date();
+        if (!endDate || endDate < startData.date()) {
             endData.date(e.date);
         }
     });
     endTimePicker.on('dp.change', function (e) {
-        startData.maxDate(e.date);
-        if (!startData.date()) {
+        var startDate = startData.date();
+        if (!startDate || startDate > endData.date()) {
             startData.date(e.date);
         }
     });
