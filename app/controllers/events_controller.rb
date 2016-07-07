f51eccf2b3254f899b2284b562edddbab2e92ab3
Lukasz Kudyba <lkudyba@hotmail.com>
2016-07-07 13:32:36 +0200

    Adjust new event form to new conference room model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,14 @@
 class EventsController < ApplicationController
 
-
-  def new
-  end
-
   def create
     Event.create(event_params)
     redirect_to root_path
   end
 
   private
+
   def event_params
-    params.require(:event).permit(:description, :location, :start_time, :end_time, :user)
+    params.require(:event).permit(:description, :location, :start_time, :end_time, :user, :conference_room_id)
   end
 
 end
