4467385a48139363feb032a49c4ba54f3060e14c
Beata Obrok <beata.obrok@u2i.com>
2016-07-08 11:53:21 +0200

    Move post request in event request spec to before block

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -6,27 +6,34 @@ RSpec.describe 'Events', type: :request do
 
     context 'with valid event' do
       let(:event) { attributes_for :event, conference_room_id: conference_room.id }
+
+      before { post events_path, params: {event: event} }
+
       it 'should respond with 200' do
-        post events_path, params: {event: event}
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
       end
     end
 
     context 'with invalid event' do
-      let(:invalid_event) { attributes_for :event, user: '', conference_room_id: conference_room.id }
+      let(:event) { attributes_for :event, user: '', conference_room_id: conference_room.id }
+
+      before { post events_path, params: {event: event} }
+
       it 'should respond with 422' do
-        post events_path, params: {event: invalid_event}
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
 
     context 'with colliding event' do
-      let(:start_time) { Time.now }
-      let!(:event) { create(:event, start_time: start_time, end_time: start_time + 1.hour, conference_room_id: conference_room.id) }
+      let!(:start_time) { Time.now }
+      let!(:valid_event) { create(:event, start_time: start_time, end_time: start_time + 1.hour, conference_room_id: conference_room.id) }
       let(:invalid_event) { attributes_for :event, start_time: start_time, end_time: start_time + 1.hour, conference_room_id: conference_room.id }
+      let(:event) { invalid_event }
+
+      before { post events_path, params: {event: event} }
+
       it 'should respond with 422' do
-        post events_path, params: {event: invalid_event}
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
