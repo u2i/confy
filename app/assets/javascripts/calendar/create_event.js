ea6f318298b9100312a64fb674aaf68516f2983f
Beata Obrok <beata.obrok@u2i.com>
2016-07-11 16:53:15 +0200

    Reformat method chaining to one method per line

--- app/assets/javascripts/calendar/create_event.js
+++ app/assets/javascripts/calendar/create_event.js
@@ -4,14 +4,18 @@ $(function () {
     }).on('ajax:error', '#create-event-form', function (e, xhr) {
         var responseObj = JSON.parse(xhr.responseText);
 
-        $('.form-group').removeClass('has-error')
-            .children('.error-text').empty();
+        $('.form-group')
+            .removeClass('has-error')
+            .children('.error-text')
+            .empty();
 
         Object.getOwnPropertyNames(responseObj)
             .forEach(function (propName) {
                 var fieldGroup = $('#' + propName + '-field-group');
-                fieldGroup.addClass('has-error')
-                    .children('.error-text').text(responseObj[propName][0]);
+                fieldGroup
+                    .addClass('has-error')
+                    .children('.error-text')
+                    .text(responseObj[propName][0]);
             });
     });
 });
