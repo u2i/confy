3f87a504bec53052540dcf8536d1a76d08f64d0b
Beata Obrok <beata.obrok@u2i.com>
2016-07-11 09:45:14 +0200

    Clear add event form after successful submission

--- app/assets/javascripts/calendar/create_event.js
+++ app/assets/javascripts/calendar/create_event.js
@@ -1,6 +1,7 @@
 $(function () {
     $(document).on('ajax:success', '#create-event-form', function () {
         $('#create-event-modal').modal('hide');
+        $('#create-event-form')[0].reset(); // Equivalent to document.getElementById("create-event-form")
     }).on('ajax:error', '#create-event-form', function (e, xhr, status, err) {
         $('#create-event-error').text('Failed to create event: ' + err).show();
     })
