decfcb89817a805980a9b70a5d5687c447e48040
Beata Obrok <beata.obrok@u2i.com>
2016-07-11 13:09:06 +0200

    [Bugfix] Moar descriptive error messages

--- app/assets/javascripts/calendar/create_event.js
+++ app/assets/javascripts/calendar/create_event.js
@@ -1,7 +1,19 @@
 $(function () {
     $(document).on('ajax:success', '#create-event-form', function () {
         $('#create-event-modal').modal('hide');
-    }).on('ajax:error', '#create-event-form', function (e, xhr, status, err) {
-        $('#create-event-error').text('Failed to create event: ' + err).show();
-    })
+    }).on('ajax:error', '#create-event-form', function (e, xhr) {
+        var responseObj = JSON.parse(xhr.responseText);
+        var errorElem = $('#create-event-error');
+        errorElem.empty();
+        var errorList = errorElem.append('<ul></ul>');
+
+        Object.getOwnPropertyNames(responseObj)
+            .forEach(function (propName) {
+                responseObj[propName].forEach(function (error) {
+                    errorList.append('<li>' + error + '</li>')
+                });
+            });
+
+        errorElem.show();
+    });
 });
--- app/models/event.rb
+++ app/models/event.rb
@@ -9,13 +9,23 @@ class Event < ApplicationRecord
   validate :no_collision
 
   def start_time_must_be_lower_than_end_time
-    errors.add(:start_time, "Can't be higher than end_time") if start_time.try(:>=, end_time)
+    errors.add(:start_time, "Start time must be lower than end time") if start_time.try(:>=, end_time)
   end
 
   def no_collision
     return unless start_time && end_time
-    return unless Event.in_span_for_conference_room(start_time, end_time, conference_room).exists?
-    errors.add(:start_time, "Can't start while another event is in progress")
+    event = Event.in_span_for_conference_room(start_time, end_time, conference_room).first
+    return unless event
+
+    event_in_progress_text = "Another event already in progress in #{conference_room.title} (#{event.start_time.strftime('%H:%M')} - #{event.end_time.strftime('%H:%M')})"
+
+    if start_time < event.end_time && start_time >= event.start_time
+      errors.add(:start_time, event_in_progress_text)
+    end
+
+    if end_time > event.start_time && end_time <= event.end_time
+      errors.add(:end_time, event_in_progress_text)
+    end
   end
 
   scope :in_span, -> (starting, ending) {
@@ -35,3 +45,4 @@ class Event < ApplicationRecord
   }
 
 end
+
