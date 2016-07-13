c181b87ea500c93f2a4388322dbd9e5e8f049cc5
Beata Obrok <beata.obrok@u2i.com>
2016-07-13 09:34:27 +0200

    Add colors to events

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-day_time = Time.now.beginning_of_week
-conference_rooms = ["Mordor", "Neverland", "Sherwood Forest", "Valhalla", "Voyager", "Winterfell", "Yellow Submarine"].map do |name|
-  params = { capacity: rand(5..20), color: "#%06x" % (rand * 0xffffff) }
+day_time = Time.new(2016, 7, 11, 8, 0, 0, "+02:00")
+colors = {'Mordor' => '#cde6f4',
+          'Neverland' => '#d2d4f6',
+          'Sherwood Forest' => '#ffebd3',
+          'Valhalla' => '#ffeec0',
+          'Voyager' => '#cdf8e7',
+          'Winterfell' => '#ffd7d3',
+          'Yellow Submarine' => '#ffdddd'}
+conference_rooms = ['Mordor', 'Neverland', 'Sherwood Forest', 'Valhalla', 'Voyager', 'Winterfell', 'Yellow Submarine'].map do |name|
+  params = {capacity: rand(5..20), color: colors[name]}
   ConferenceRoom.where(title: name).first_or_create(params).tap { |cr| cr.update(params) }
 end
 
@@ -20,12 +27,12 @@ event_id = 0
     start_time = day_time + j.hours * 3
     end_time = start_time + rand(1..2).hours + 30*(rand(0..1)).minutes
     params = {
-        start_time: start_time,
-        end_time: end_time,
-        name: Faker::Company.name,
-        description: Faker::Company.catch_phrase,
-        conference_room: conference_rooms.sample,
-        user: Faker::Name.name
+      start_time: start_time,
+      end_time: end_time,
+      name: Faker::Company.name,
+      description: Faker::Company.catch_phrase,
+      conference_room: conference_rooms.sample,
+      user: Faker::Name.name
     }
     if (event = Event.all[event_id])
       event.update(params)
