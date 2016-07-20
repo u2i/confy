2eae0695a510a13eb4dfc6438c2f8902256c67af
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-07-20 12:16:31 +0200

    Authentication request spec

---
+++ spec/requests/authentications_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Authentications', type: :request do
+  describe 'GET /oauth2callback' do
+    context 'valid credentials' do
+      let(:google_credentials) { 'sample string' }
+
+      before do
+        allow(GoogleOauth).to receive(:get_user_credentials) { google_credentials }
+        get oauth2callback_path, params: { code: 'sample_code' }
+      end
+
+      it 'redirects to root_path and sets session' do
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'invalid credentials' do
+      let(:example_page) { 'http://example.com' }
+      before do
+        allow(GoogleOauth).to receive(:get_user_credentials) { raise ArgumentError, 'Test error' }
+        allow(GoogleOauth).to receive(:request_code_uri) { example_page }
+        get oauth2callback_path, params: { code: 'sample_code' }
+      end
+
+      it 'redirects to root_path and sets session' do
+        expect(response).to redirect_to example_page
+      end
+    end
+  end
+end
