eccc199dc7914427c863106d44ecf88b73b3329f
Lukasz Kudyba <lkudyba@hotmail.com>
2016-07-26 15:23:45 +0200

    React wrapper component for app

---
+++ client/app/components/AppContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Calendar from './calendar/Calendar'
+import SideNav from './SideNav'
+
+export default class AppContainer extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-12 col-md-10">
+          <SideNav />
+          <Calendar {...this.props} />
+        </div>
+      </div>
+    )
+  }
+}
