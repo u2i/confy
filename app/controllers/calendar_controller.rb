021b5c44c2fc6031f56c207126dc5ef6651b2d04
Quirk0o <beata@obrok.eu>
2016-07-27 13:01:20 +0200

    Update events index to fetch events from Google

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,6 +3,8 @@ require 'google/api_client/client_secrets'
 
 class CalendarController < ApplicationController
   include GoogleAuthentication
+  include GoogleEventListing
+  include TimeInterval
 
   before_action :check_authentication
   before_action :refresh_token
@@ -10,14 +12,7 @@ class CalendarController < ApplicationController
 
   # Index for showing events from Google calendar
   def index
-    @events = GoogleEvent.list_events(
-      session[:credentials],
-      @week_start.beginning_of_day.to_datetime,
-      @week_end.end_of_day.to_datetime
-    ).map do |_wday, events|
-      build_blocks(events)
-    end.flatten!(1)
-
+    @events = list_events(@week_start, @week_end)
     create_calendar_props
   rescue ArgumentError
     session.delete(:credentials)
@@ -27,32 +22,15 @@ class CalendarController < ApplicationController
   private
 
   def create_calendar_props
-    @props = {events: @events, days: @days, times: @times, unitEventLength: CalendarHelper::UNIT_EVENT_LENGTH}
+    @props = {initialEvents: @events, days: @days, times: @times, unitEventLength: CalendarHelper::UNIT_EVENT_LENGTH}
   end
 
   def load_dates_and_rooms
-    @week_start, @week_end = build_week_boundaries
+    @week_start, @week_end = build_week_boundaries(params[:date])
     @days = (@week_start..@week_end).to_a
     start_time = Time.now.at_beginning_of_day
     end_time = Time.now.at_end_of_day
     @times = time_interval(start_time, end_time, 30.minutes)
     @conference_rooms = ConferenceRoom.all
-  end
-
-  def time_interval(start_time, end_time, step)
-    (start_time.to_i..end_time.to_i).step(step).collect { |time| Time.at time }
-  end
-
-  def week_start
-    params[:date].present? ? Date.parse(params[:date]).beginning_of_week : Date.today.beginning_of_week
-  end
-
-  def build_week_boundaries
-    week_end = week_start + CalendarHelper::WEEK_LENGTH - 1
-    [week_start, week_end]
-  end
-
-  def build_blocks(events)
-    EventGrouper.new(events.sort_by! { |e| e[:end][:date_time] }).call
   end
 end
---
+++ app/controllers/concerns/google_event_listing.rb
@@ -0,0 +1,15 @@
+module GoogleEventListing
+  extend ActiveSupport::Concern
+
+  def list_events(starting, ending)
+    events = GoogleEvent.list_events(session[:credentials], starting.to_datetime, ending.to_datetime)
+    grouped_events = events.map do |_wday, events|
+      build_groups(events)
+    end
+    grouped_events.flatten!(1)
+  end
+
+  def build_groups(events)
+    EventGrouper.new(events.sort_by! { |e| e[:end][:date_time] }).call
+  end
+end
