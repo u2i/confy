a40aadea08f73d783a3dee3befcb757800cd5410
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-07-28 14:38:37 +0200

    Added jsdom feature

--- client/test/SampleTest.jsx
+++ client/test/SampleTest.jsx
@@ -3,14 +3,19 @@ import { Checkbox } from 'react-bootstrap'
 import { mount, shallow } from 'enzyme';
 import RoomFilters from '../app/components/calendar/RoomFilters'
 import chai from 'chai'
+import jsdom from 'mocha-jsdom'
 
 var expect = chai.expect;
 
 describe('<RoomFilters />', () => {
 
+    jsdom();
+
     it('renders Checkbox component', () => {
         let conferenceRooms = [{id: 12, title: "ala", color:"#000000"}];
         const wrapper = shallow(<RoomFilters add={a => a+2} delete={b => b-2} conferenceRooms={conferenceRooms}/>);
+        const wrapper2 = mount(<RoomFilters add={a => a+2} delete={b => b-2} conferenceRooms={conferenceRooms}/>);
+        expect(wrapper2.find(Checkbox)).to.have.length(1);
         expect(wrapper.find(Checkbox)).to.have.length(1);
         expect(wrapper.is('div')).to.eq(true);
     });
