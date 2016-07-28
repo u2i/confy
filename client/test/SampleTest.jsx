5570135458cd835d97d41ffc0d8681d629cc5b39
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-07-28 15:47:36 +0200

    Fixed example test

--- client/test/SampleTest.jsx
+++ client/test/SampleTest.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Checkbox } from 'react-bootstrap'
 import { mount, shallow } from 'enzyme';
-import RoomFilters from '../app/components/calendar/RoomFilters'
+import RoomFilters from '../app/components/calendar/filters/RoomFilters'
+import Filter from '../app/components/calendar/filters/Filter'
 import chai from 'chai'
 import jsdom from 'mocha-jsdom'
 
@@ -11,10 +12,10 @@ describe('<RoomFilters />', () => {
 
     jsdom();
 
-    it('renders Checkbox component', () => {
+    it('renders Filter component', () => {
         let conferenceRooms = [{id: 12, title: "ala", color:"#000000"}];
-        const wrapper = shallow(<RoomFilters add={a => a+2} delete={b => b-2} conferenceRooms={conferenceRooms}/>);
-        expect(wrapper.find(Checkbox)).to.have.length(1);
+        const wrapper = shallow(<RoomFilters onEnabled={a => a+2} onDisabled={b => b-2} conferenceRooms={conferenceRooms}/>);
+        expect(wrapper.find(Filter)).to.have.length(1);
         expect(wrapper.is('div')).to.eq(true);
     });
 
