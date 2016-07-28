d188a20d4f0ae5fc10a3b96b862056aed9e859ba
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-07-28 11:14:57 +0200

    Nyan Cat + setup refactor

--- client/test/SampleTest.jsx
+++ client/test/SampleTest.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Checkbox } from 'react-bootstrap'
 import { mount, shallow } from 'enzyme';
 import RoomFilters from '../app/components/calendar/RoomFilters'
+import chai from 'chai'
 
-import chai from 'chai';
+var expect = chai.expect;
 
 describe('<RoomFilters />', () => {
 
     it('renders Checkbox component', () => {
         let conferenceRooms = [{id: 12, title: "ala", color:"#000000"}];
         const wrapper = shallow(<RoomFilters add={a => a+2} delete={b => b-2} conferenceRooms={conferenceRooms}/>);
-        chai.expect(wrapper.find(Checkbox)).to.have.length(1);
-        chai.expect(wrapper.is('div')).to.eq(true);
+        expect(wrapper.find(Checkbox)).to.have.length(1);
+        expect(wrapper.is('div')).to.eq(true);
     });
 
 });
