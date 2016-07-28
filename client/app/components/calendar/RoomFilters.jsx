4cdf35cee9fe9b9580cfc624f506e841030ecb80
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-07-28 10:26:11 +0200

    Code refactor

--- client/app/components/calendar/RoomFilters.jsx
+++ client/app/components/calendar/RoomFilters.jsx
@@ -1,17 +1,21 @@
 import React  from 'react'
-
-import { Checkbox, Label } from 'react-bootstrap'
-
+import FilterBox from './FilterBox'
+import { Checkbox } from 'react-bootstrap'
 import './filters.scss'
 
 export default class RoomFilters extends React.Component {
+
+    constructor(){
+        super();
+        this._handleCheckbox = this._handleCheckbox.bind(this);
+    }
     
-    _handleCheckbox(event){
+    _handleCheckbox(event, conferenceRoomId){
         if(!event.target.checked){
-            this.props.add(parseInt(event.target.value));
+            this.props.add(conferenceRoomId);
         }
         else{
-            this.props.delete(parseInt(event.target.value));
+            this.props.delete(conferenceRoomId);
         }
     }
 
@@ -19,10 +23,8 @@ export default class RoomFilters extends React.Component {
         let filters = this.props.conferenceRooms.map(
             (conferenceRoom) => {
                 return (
-                    <div className="filter-box" style={{backgroundColor: conferenceRoom.color}}>
-                        <Checkbox value={conferenceRoom.id} onChange={this._handleCheckbox.bind(this)}
-                                  defaultChecked inline>{conferenceRoom.title}</Checkbox>
-                    </div>
+                    <FilterBox conferenceRoom={conferenceRoom}
+                               handler={(e) => this._handleCheckbox(e, conferenceRoom.id) }/>
                 )}
         );
         return (
