92b9ca1cef652d1baf8fd5cba4b01a732349c93a
Quirk0o <beata@obrok.eu>
2016-07-28 11:50:40 +0200

    [Fix] layout

--- client/app/components/AppContainer.jsx
+++ client/app/components/AppContainer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Grid, Col } from 'react-bootstrap';
 import Calendar from './calendar/Calendar';
 import SideNav from './layout/SideNav';
 
 export default (props) => (
-  <div className="container">
-    <div className="col-xs-12 col-md-10">
+  <Grid>
+    <Col xs={12} md={2}>
       <SideNav />
+    </Col>
+    <Col xs={12} md={10}>
       <Calendar {...props} />
-    </div>
-  </div>
+    </Col>
+  </Grid>
 );
--- client/app/components/layout/SideNav.jsx
+++ client/app/components/layout/SideNav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 export default () => (
-  <aside className="col-xs-12 col-md-2 sidebar">
+  <aside className="sidebar">
     <Button bsStyle="primary" className="btn-block">Create Event</Button>
     <Button bsStyle="primary" className="btn-block">Home</Button>
     <Button bsStyle="primary" className="btn-block">Next Week</Button>
