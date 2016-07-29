944a735c6239e9c4c5f21ebca04006d990aa0f51
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-07-29 10:24:08 +0200

    Hound issues

--- client/test/FilterComponent-test.jsx
+++ client/test/FilterComponent-test.jsx
@@ -12,55 +12,55 @@ describe('<Filter />', () => {
 
     jsdom();
 
-    let sampleColor = "#000000";
-    let sampleTitle = 'sample_title';
-    let sampleConferenceRoomId = 1;
-    let sampleCapacity = 1;
-    let conferenceRoom = {
-        id: sampleConferenceRoomId,
-        title: sampleTitle,
-        color: sampleColor,
-        capacity: sampleCapacity
+    const color = "#000000";
+    const title = 'sample_title';
+    const id = 1;
+    const capacity = 1;
+    const conferenceRoom = {
+        id,
+        title,
+        color,
+        capacity
     };
-    let defaultProps = {
-        conferenceRoom: conferenceRoom,
+    const defaultProps = {
+        conferenceRoom,
         onEnabled: () => {},
         onDisabled: () => {},
         enabled: false
     };
-    let defaultWrapper = shallow(<Filter {...defaultProps} />);
+    const defaultWrapper = shallow(<Filter {...defaultProps} />);
 
     it('renders Checkbox component', () => {
         expect(defaultWrapper.find(Checkbox)).to.have.length(1);
     });
 
     it("sets .filter-box backgroundColor", () => {
-        expect(defaultWrapper.find('.filter-box').props().style.backgroundColor).to.eq(sampleColor);
+        expect(defaultWrapper.find('.filter-box').props().style.backgroundColor).to.eq(color);
     });
 
     it('puts conference room title inside Checkbox component', () => {
-        expect(defaultWrapper.find(Checkbox).children().text()).to.eq(sampleTitle);
+        expect(defaultWrapper.find(Checkbox).children().text()).to.eq(title);
     });
 
     it("triggers appropriate handler based on 'enabled' prop", () => {
-        let onEnabled = sinon.spy();
-        let onDisabled = sinon.spy();
-        let props = {
-            conferenceRoom: conferenceRoom,
-            onEnabled: onEnabled,
-            onDisabled: onDisabled,
+        const onEnabled = sinon.spy();
+        const onDisabled = sinon.spy();
+        const props = {
+            conferenceRoom,
+            onEnabled,
+            onDisabled,
             enabled: false
         };
-        const wrapper = mount(<Filter {...props}/>);
+        const wrapper = mount(<Filter {...props} />);
 
         wrapper.find('input').simulate('change');
         expect(onDisabled).to.have.property('callCount', 1);
-        expect(onDisabled.calledWith(sampleConferenceRoomId)).to.eq(true);
+        expect(onDisabled.calledWith(id)).to.eq(true);
 
-        wrapper.setProps({enabled: true});
+        wrapper.setProps({ enabled: true });
 
         wrapper.find('input').simulate('change');
         expect(onEnabled).to.have.property('callCount', 1);
-        expect(onEnabled.calledWith(sampleConferenceRoomId)).to.eq(true);
+        expect(onEnabled.calledWith(id)).to.eq(true);
     });
 });
