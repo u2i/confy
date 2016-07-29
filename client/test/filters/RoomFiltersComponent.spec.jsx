f38555d7da2eaff28d9007f48f0e275d23607638
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-07-29 12:10:46 +0200

    Hound fix

--- client/test/filters/RoomFiltersComponent.spec.jsx
+++ client/test/filters/RoomFiltersComponent.spec.jsx
@@ -10,21 +10,21 @@ describe('<RoomFilters />', () => {
   const [id1, id2, id3] = [1,2,3];
   const [title1, title2, title3] = ['title1', 'title2', 'title3'];
   const conferenceRoom1 = {
-    id: id1,
+    id:    id1,
     title: title1
   };
   const conferenceRoom2 = {
-    id: id2,
+    id:    id2,
     title: title2
   };
   const conferenceRoom3 = {
-    id: id3,
+    id:    id3,
     title: title3
   };
   const conferenceRooms = [conferenceRoom1, conferenceRoom2, conferenceRoom3];
   const props = {
     conferenceRooms,
-    onEnabled: () => {},
+    onEnabled:  () => {},
     onDisabled: () => {}
   };
   const defaultWrapper = shallow(<RoomFilters {...props} />);
