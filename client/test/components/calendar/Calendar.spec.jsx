8c8a909cf75a402cc352f258a915f3f8b42a70e6
Quirk0o <beata@obrok.eu>
2016-07-29 11:56:56 +0200

    Tests for Calendar component

---
+++ client/test/components/calendar/Calendar.spec.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import { Table } from 'react-bootstrap';
+import Calendar from '../../../app/components/calendar/Calendar';
+import CalendarHeader from '../../../app/components/calendar/CalendarHeader';
+import CalendarRow from '../../../app/components/calendar/CalendarRow';
+import RoomFilters from '../../../app/components/calendar/filters/RoomFilters';
+
+describe('<Calendar />', () => {
+  const days = ['2016-07-28', '2016-07-29'];
+  const times = ['8:00', '9:00'];
+  const unitEventLength = 0;
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Calendar days={days} times={times} unitEventLengthInSeconds={unitEventLength} />);
+  });
+
+  describe('#render', () => {
+    it('renders calendar <Table />', () => {
+      const table = wrapper.find(Table);
+      expect(table).to.have.length(1);
+      expect(table.hasClass('calendar')).to.be.true;
+    });
+
+    it('renders <RoomFilters /> components', () => {
+      expect(wrapper.find(RoomFilters)).to.have.lengthOf(1);
+    });
+
+    it('renders a table column for each day', () => {
+      expect(wrapper.find(CalendarHeader)).to.have.lengthOf(days.length);
+    });
+
+    it('renders a table row for each time', () => {
+      expect(wrapper.find(CalendarRow)).to.have.lengthOf(times.length);
+    });
+  });
+
+  describe('state', () => {
+    it('initializes with empty filter list', () => {
+      expect(wrapper.state('filteredRooms').size).to.equal(0);
+    });
+
+    it('updates filter set after enabling a filter', () => {
+      wrapper.find(RoomFilters).simulate('enabled', 1);
+      expect(wrapper.state('filteredRooms').size).to.equal(1);
+      expect(wrapper.state('filteredRooms').includes(1)).to.be.true;
+    });
+
+    it('updates filter set after disabling a filter', () => {
+      wrapper.find(RoomFilters).simulate('enabled', 1);
+      wrapper.find(RoomFilters).simulate('disabled', 1);
+      expect(wrapper.state('filteredRooms').size).to.equal(0);
+      expect(wrapper.state('filteredRooms').includes(1)).to.be.false;
+    });
+  });
+});
