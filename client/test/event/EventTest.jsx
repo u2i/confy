064a3aa7fb60637e4c69f2da4658deedc64f3f08
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-07-29 10:34:46 +0200

    Improved styles

--- client/test/event/EventTest.jsx
+++ client/test/event/EventTest.jsx
@@ -10,25 +10,25 @@ var expect = chai.expect;
 
 describe('<Event />', () => {
   const eventSummary = 'Sample Event';
-  const creator = {email:'creator@example.com',self:true};
+  const creator = { email: 'creator@example.com',self: true };
   const sampleRoom = {
-    id:8,
-    capacity:10,
-    title:'Narnia One',
-    color:'#dffabd',
-    email:'email@resource.calendar.google.com'
+    id: 8,
+    capacity: 10,
+    title: 'Narnia One',
+    color: '#dffabd',
+    email: 'email@resource.calendar.google.com'
   };
   const sampleEvent = {
-    attendees:[{response_status:'needsAction', self:true}],
+    attendees: [{ response_status: 'needsAction', self: true }],
     creator: creator,
-    end:{date_time:'2016-07-25T02:30:00.000+02:00'},
-    id:'7utc9k4fds8kf2734q72dsoq8c',
-    start:{date_time:'2016-07-25T00:30:00.000+02:00'},
+    end: { date_time: '2016-07-25T02:30:00.000+02:00' },
+    id: '7utc9k4fds8kf2734q72dsoq8c',
+    start: { date_time: '2016-07-25T00:30:00.000+02:00' },
     conference_room: sampleRoom,
-    start_timestamp:1469397600,
-    end_timestamp:1469406600,
+    start_timestamp: 1469397600,
+    end_timestamp: 1469406600,
     summary: eventSummary
-  }
+  };
   const containerHeight = 30;
   const unitEventLengthInSeconds = 30 * 60;
   const timeFormat = 'HH:mm'
