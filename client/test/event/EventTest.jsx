125ca51d9f8dc6c16086d99fb6d6fdb76bf7ad7f
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-07-29 10:08:38 +0200

    Test improvements

--- client/test/event/EventTest.jsx
+++ client/test/event/EventTest.jsx
@@ -10,23 +10,25 @@ var expect = chai.expect;
 describe('<Event />', () => {
 
 
-    const sampleEvent = {
-        attendees:[{response_status:"needsAction", self:true}],
-        creator:{email:"creator@example.com",self:true},
-        end:{date_time:"2016-07-25T02:30:00.000+02:00"},
-        id:"7utc9k4ft8kf22734q72ihoq8c",
-        start:{date_time:"2016-07-25T00:30:00.000+02:00"},
-        conference_room:
-            {
+    const eventSummary = "Sample Event";
+    const creator = {email:"creator@example.com",self:true};
+    const sampleRoom = {
                 id:8,
                 capacity:10,
                 title:"Narnia One",
                 color:"#dffabd",
                 email:"email@resource.calendar.google.com"
-            },
+            };
+    const sampleEvent = {
+        attendees:[{response_status:"needsAction", self:true}],
+        creator: creator,
+        end:{date_time:"2016-07-25T02:30:00.000+02:00"},
+        id:"7utc9k4fds8kf2734q72dsoq8c",
+        start:{date_time:"2016-07-25T00:30:00.000+02:00"},
+        conference_room: sampleRoom,
         start_timestamp:1469397600,
         end_timestamp:1469406600,
-        summary: "Sample Event"
+        summary: eventSummary
     }
     const containerHeight = 30;
     const unitEventLengthInSeconds = 30 * 60;
@@ -35,18 +37,19 @@ describe('<Event />', () => {
     jsdom();
 
     it('renders correctly', () => {
-        const wrapper = mount(<Event event={sampleEvent} containerHeight={containerHeight} unitEventLengthInSeconds={unitEventLengthInSeconds} timeFormat={timeFormat}/>);
+        const wrapper = shallow(<Event event={sampleEvent} containerHeight={containerHeight} unitEventLengthInSeconds={unitEventLengthInSeconds} timeFormat={timeFormat}/>);
         expect(wrapper.find("div")).to.have.length(5);
         expect(wrapper.find(".event").props().style.height).to.eq(150);
         expect(wrapper.find(".event-time").text()).to.include("00:30");
-        expect(wrapper.find(".event-name").text()).to.include("Sample Event");
-        expect(wrapper.find(".event-user").text()).to.include("creator@example.com");
-        expect(wrapper.find(".event-location").text()).to.include("Narnia One");
+        expect(wrapper.find(".event-name").text()).to.include(eventSummary);
+        expect(wrapper.find(".event-user").text()).to.include(creator.email);
+        expect(wrapper.find(".event-location").text()).to.include(sampleRoom.title);
     });
 
     it('renders correctly with other length', () => {
         let eventClone = JSON.parse(JSON.stringify(sampleEvent));
         eventClone.end_timestamp += 60 * 60; // 1 hour
+        eventClone.end.date_time = "2016-07-25T03:30:00.000+02:00";
         const wrapper = mount(<Event event={eventClone} containerHeight={containerHeight} unitEventLengthInSeconds={unitEventLengthInSeconds} timeFormat={timeFormat}/>);
         expect(wrapper.find(".event").props().style.height).to.eq(210);
     })
