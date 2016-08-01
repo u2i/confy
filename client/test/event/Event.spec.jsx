205376f0367af6d318b81727b8d1c04d0f5a914d
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-08-01 15:55:44 +0200

    Use factories in specs

--- client/test/event/Event.spec.jsx
+++ client/test/event/Event.spec.jsx
@@ -3,37 +3,19 @@ import {shallow, mount} from 'enzyme';
 import Event from '../../app/components/calendar/event/Event';
 import DeleteButton from '../../app/components/calendar/event/DeleteButton';
 import {expect} from 'chai';
-import {_} from 'lodash';
+import EventFactory from '../factories/Event'
 
 describe('<Event />', () => {
-  const eventSummary = 'Sample Event';
-  const creator = {email: 'creator@example.com', self: true};
-  const sampleRoom = {
-    id: 8,
-    capacity: 10,
-    title: 'Narnia One',
-    color: '#dffabd',
-    email: 'email@resource.calendar.google.com'
-  };
-  const sampleEvent = {
-    creator,
-    attendees: [{response_status: 'needsAction', self: true}],
-    end: {date_time: '2016-07-25T02:30:00.000+02:00'},
-    id: '7utc9k4fds8kf2734q72dsoq8c',
-    start: {date_time: '2016-07-25T00:30:00.000+02:00'},
-    conference_room: sampleRoom,
-    start_timestamp: 1469397600,
-    end_timestamp: 1469406600,
-    summary: eventSummary
-  };
   const containerHeight = 30;
   const unitEventLengthInSeconds = 30 * 60;
   const timeFormat = 'HH:mm';
   describe('creator.self', () => {
     it('renders delete button when self correctly', () => {
+      let event = EventFactory.build();
+      event.creator.self = true;
       const wrapper = mount(
         <Event
-          event={sampleEvent}
+          event={event}
           containerHeight={containerHeight}
           unitEventLengthInSeconds={unitEventLengthInSeconds}
           timeFormat={timeFormat}/>
@@ -43,13 +25,11 @@ describe('<Event />', () => {
   });
   describe('undefined creator.self', () => {
     it('does not render delete button when not self', () => {
-      let creatorClone = _.cloneDeep(creator);
-      creatorClone.self = undefined;
-      let eventClone = _.cloneDeep(sampleEvent);
-      eventClone.creator = creatorClone;
+      let event = EventFactory.build();
+      event.creator.self = undefined;
       const wrapper = mount(
         <Event
-          event={eventClone}
+          event={event}
           containerHeight={containerHeight}
           unitEventLengthInSeconds={unitEventLengthInSeconds}
           timeFormat={timeFormat}/>
