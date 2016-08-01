d06b0a23f2036c69b0c55228daba24f1be47f0f4
Lukasz Kudyba <lkudyba@hotmail.com>
2016-08-01 16:10:54 +0200

    Code style

--- client/app/components/AppContainer.jsx
+++ client/app/components/AppContainer.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Grid, Col } from 'react-bootstrap';
 import Calendar from './calendar/Calendar';
 import SideNav from './layout/SideNav';
 import CreateEventModal from './CreateEventModal';
 import _ from 'lodash';
 
+const { array } = PropTypes;
 
 export default class AppContainer extends React.Component {
+  static propTypes = {
+    conferenceRooms: array
+  };
+
   constructor() {
     super();
     this.state = {
@@ -14,14 +19,6 @@ export default class AppContainer extends React.Component {
     };
 
     _.bindAll(this, ['openModal', 'closeModal']);
-  }
-
-  openModal() {
-    this.setState({ showModal: true });
-  }
-
-  closeModal() {
-    this.setState({ showModal: false });
   }
 
   render() {
@@ -40,6 +37,14 @@ export default class AppContainer extends React.Component {
           closeModal={this.closeModal}
           conferenceRooms={this.props.conferenceRooms} />
       </div>
-    )
+    );
+  }
+
+  openModal() {
+    this.setState({ showModal: true });
+  }
+
+  closeModal() {
+    this.setState({ showModal: false });
   }
 }
