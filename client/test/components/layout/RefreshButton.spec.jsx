5ef66e8cdc902dd25ff9d41ba2f6ab42564ee291
Quirk0o <beata@obrok.eu>
2016-08-02 11:31:59 +0200

    Test for refresh button

---
+++ client/test/components/layout/RefreshButton.spec.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { Button, Glyphicon } from 'react-bootstrap';
+import RefreshButton from '../../../app/components/layout/RefreshButton';
+
+describe('<RefreshButton />', () => {
+  const refreshSpy = sinon.spy();
+
+  it('invokes callback on refresh', () => {
+    const wrapper = shallow(<RefreshButton onRefresh={refreshSpy} />);
+    wrapper.find(Button).forEach(button => button.simulate('click'));
+    expect(refreshSpy).to.have.been.calledOnce();
+  });
+
+  it('renders icon with .spin class when animated', () => {
+    const wrapper = shallow(<RefreshButton animate />);
+    expect(wrapper.find(Glyphicon)).to.have.className('spin');
+  })
+});
