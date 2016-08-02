cf8de1502dcc3c5469338304ee873839ea19d5bf
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-08-02 12:45:12 +0200

    Removed predicate from eventGroupContaining function

--- client/app/helpers/EventHelper.jsx
+++ client/app/helpers/EventHelper.jsx
@@ -1,18 +1,11 @@
 export const SECONDS_IN_DAY = 24 * 60 * 60;
 
-export function eventStartsAt(timestamp) {
+function eventStartsAt(timestamp) {
   return (event) => event.start_timestamp === timestamp;
-}
-
-export function groupInDay(group, timestamp) {
-  const lastEvent = group[group.length - 1];
-  return Math.abs(lastEvent.start_timestamp - timestamp) < SECONDS_IN_DAY;
 }
 
 export function eventGroupContaining(events, timestamp) {
   return events.find(group =>
-    group.length &&
-    groupInDay(group, timestamp) &&
     group.some(eventStartsAt(timestamp))
   );
 }
--- client/test/helpers/EventHelper.spec.jsx
+++ client/test/helpers/EventHelper.spec.jsx
@@ -3,35 +3,6 @@ import { expect } from 'chai';
 import Event from '../factories/Event';
 
 describe('EventHelper', () => {
-  describe('#eventStartsAt()', () => {
-    const timestamp = (new Date()).getTime() / 1000;
-    const event = Event.build();
-
-    it('returns a function', () => {
-      expect(EventHelper.eventStartsAt(timestamp)).to.be.a('function');
-    });
-
-    it('returns function that checks if event starts at the specific time', () => {
-      const returnedFunction = EventHelper.eventStartsAt(event.start_timestamp);
-      expect(returnedFunction(event)).to.eq(true);
-    });
-  });
-  describe('#groupInDay()', () => {
-    const event1 = Event.build({}, { start_time: new Date(2016, 7, 25, 4, 0, 0) });
-    const event2 = Event.build({}, { start_time: new Date(2016, 7, 25, 12, 0, 0) });
-    const event3 = Event.build({}, { start_time: new Date(2016, 7, 26, 4, 0, 0) });
-    const event4 = Event.build({}, { start_time: new Date(2016, 7, 26, 12, 0, 0) });
-    const timestamp = (new Date(2016, 7, 25, 0, 0, 0)).getTime() / 1000;
-    const group1 = [event1, event2];
-    const group2 = [event3, event4];
-    it('returns true if given group of events sorted by starting time belongs to a specific day', () => {
-      expect(EventHelper.groupInDay(group1, timestamp)).to.eq(true);
-    });
-
-    it('returns false if given group of events sorted by starting time doest not belong to a specific day', () => {
-      expect(EventHelper.groupInDay(group2, timestamp)).to.eq(false);
-    });
-  });
   describe('#eventGroupContaining()', () => {
     const event1 = Event.build({}, { start_time: new Date(2016, 7, 25, 4, 0, 0) });
     const event2 = Event.build({}, { start_time: new Date(2016, 7, 25, 16, 0, 0) });
