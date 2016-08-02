5b593c6687dacf0de066222d753631f315e41a4c
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-08-02 14:59:00 +0200

    Use axios interceptor

--- client/app/sources/EventSource.js
+++ client/app/sources/EventSource.js
@@ -2,18 +2,27 @@ import axios from 'axios';
 
 const EVENT_PATH = '/events';
 
+const getCSRFToken = (config) => {
+  const token = document.querySelector('meta[name="csrf-token"]').content;
+  if (config.headers) {
+    config.headers['X-CSRF-Token'] = token;
+  } else {
+    config.headers = { 'X-CSRF-Token': token };
+  }
+  return config;
+};
+
+axios.interceptors.request.use(getCSRFToken, (error) => Promise.reject(error));
+
+
 const EventSource = {
   fetch(params) {
     return axios.get(EVENT_PATH, { params });
   },
   remove(id) {
-    const token = document.querySelector('meta[name="csrf-token"]').content;
     axios({
       method: 'DELETE',
       url: `${EVENT_PATH}/${id}`,
-      headers: {
-        'X-CSRF-Token': token
-      }
     }).then(() => {}).catch(() => {
       alert('Server error');
     });
