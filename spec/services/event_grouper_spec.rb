e2fb1fbf346bb2c8cc550bd84ad8e1a2be780c03
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-08-04 14:21:44 +0200

    Fixed bug in EventGrouper.floor_time

--- spec/services/event_grouper_spec.rb
+++ spec/services/event_grouper_spec.rb
@@ -72,4 +72,40 @@ RSpec.describe EventGrouper do
       end
     end
   end
+
+  describe '.floor_time' do
+    context 'granularity past hour' do
+      let(:time) { Time.now.beginning_of_hour + EventGrouper::GRANULARITY }
+
+      it 'returns argument' do
+        expect(described_class.floor_time(time)).to eq time
+      end
+    end
+
+    context 'half granularity past hour' do
+      let(:time) { Time.now.beginning_of_hour + EventGrouper::GRANULARITY / 2 }
+
+      it 'floors time' do
+        expect(described_class.floor_time(time)).to eq time.beginning_of_hour
+      end
+    end
+  end
+
+  describe '.ceil_time' do
+    context 'granularity past hour' do
+      let(:time) { Time.now.beginning_of_hour + EventGrouper::GRANULARITY }
+
+      it 'returns argument' do
+        expect(described_class.ceil_time(time)).to eq time
+      end
+    end
+
+    context 'half granularity past hour' do
+      let(:time) { Time.now.beginning_of_hour + EventGrouper::GRANULARITY / 2 }
+
+      it 'ceils time' do
+        expect(described_class.ceil_time(time)).to eq (time.beginning_of_hour + EventGrouper::GRANULARITY)
+      end
+    end
+  end
 end
