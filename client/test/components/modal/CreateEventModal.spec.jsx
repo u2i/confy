3205c98276a8c4e375c8359ffe5601579ac2aef6
Lukasz Kudyba <lkudyba@hotmail.com>
2016-08-04 14:41:42 +0200

    CreateEventModal tests

---
+++ client/test/components/modal/CreateEventModal.spec.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {Modal} from "react-bootstrap";
+import {mount} from "enzyme";
+import {expect} from "chai";
+import CreateEventModal from "components/modal/CreateEventModal";
+import ConferenceRoom from "test/factories/ConferenceRoom";
+import ModalHeader from "components/modal/layout/ModalHeader";
+import ModalBody from "components/modal/layout/ModalBody";
+import ModalFooter from "components/modal/layout/ModalFooter";
+import ErrorField from "components/modal/layout/ErrorField";
+
+describe('<CreateEventModal />', () => {
+  const wrapper = mount(<CreateEventModal
+    conferenceRooms={[ConferenceRoom.build()]} />);
+
+  it('renders <Modal />', () => {
+    expect(wrapper.find(Modal)).to.exist;
+  });
+
+  it('renders <ModalHeader />', () => {
+    expect(wrapper.find(ModalHeader)).to.exist;
+  });
+
+  it('renders <ModalBody />', () => {
+    expect(wrapper.find(ModalBody)).to.exist;
+  });
+
+  it('renders <ModalFooter />', () => {
+    expect(wrapper.find(ModalFooter)).to.exist;
+  });
+
+  it('renders <ErrorField />', () => {
+    expect(wrapper.find(ErrorField)).to.exist;
+  });
+});
