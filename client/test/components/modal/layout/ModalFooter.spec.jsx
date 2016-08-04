b2551766c9602a0deb5f052d9c58fb3420a06a38
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-08-04 10:03:22 +0200

    Improving ModalFooter specs

--- client/test/components/modal/layout/ModalFooter.spec.jsx
+++ client/test/components/modal/layout/ModalFooter.spec.jsx
@@ -5,12 +5,21 @@ import { expect } from 'chai';
 import ModalFooter from 'components/modal/layout/ModalFooter';
 
 describe('<ModalFooter />', () => {
-  const wrapper = shallow(<ModalFooter />);
+  const props = { closeModal: () => {}, saveChanges: () => {} };
+  const wrapper = shallow(<ModalFooter {...props} />);
   it('renders <Modal.Footer />', () => {
     expect(wrapper.find(Modal.Footer)).to.have.lengthOf(1);
   });
   
   it('renders 2 <Button /> inside <Modal.Footer />', () => {
     expect(wrapper.find(Modal.Footer).find(Button)).to.have.lengthOf(2);
+  });
+
+  it('renders <Button /> with "Close" string inside', () => {
+    expect(wrapper.find(Button).findWhere(n => n.text() === "Close")).to.have.lengthOf(1);
+  });
+
+  it('renders <Button /> with "Save changes" string inside', () => {
+    expect(wrapper.find(Button).findWhere(n => n.text() === "Save changes")).to.have.lengthOf(1);
   });
 });
