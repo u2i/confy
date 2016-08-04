80caea73a446784f6ea56273287141cc3aac12e2
Quirk0o <beata@obrok.eu>
2016-08-04 22:39:45 +0200

    Fix AppContainer tests and add Calendar test for scrolling

--- client/test/common.js
+++ client/test/common.js
@@ -1,14 +1,36 @@
 
-require('sinon');
-require('sinon-as-promised');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import sinon from 'sinon';
 import chai from 'chai';
+import proxyquire from 'proxyquire';
+import shared from 'mocha-shared';
+
 import chaiEnzyme from 'chai-enzyme';
 import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
 import dirtyChai from 'dirty-chai';
 
+require('sinon-as-promised');
+
 chai.use(chaiEnzyme());
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 chai.use(dirtyChai);
+
+shared.setup('stub ReactDOM.findDOMNode', ({ cb } = {}) => {
+  before(() => {
+    const node = document.createElement('div');
+    const stub = sinon.stub(ReactDOM, 'findDOMNode').returns(node);
+    proxyquire('react-dom', ReactDOM);
+    if (cb) {
+      cb(node, stub);
+    }
+  });
+
+  after(() => {
+    ReactDOM.findDOMNode.restore();
+  });
+});
+
