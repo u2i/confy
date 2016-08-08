a4d0fc5a62600c25a6d162d84f31e4d39fb3070c
Lukasz Kudyba <lkudyba@hotmail.com>
2016-08-08 14:20:05 +0200

    Code style

--- client/app/components/modal/layout/body/FormDateField.jsx
+++ client/app/components/modal/layout/body/FormDateField.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import moment from 'moment';
-import { FormGroup, ControlLabel } from 'react-bootstrap';
-import DateTimeField from 'react-bootstrap-datetimepicker';
-import { If } from 'react-if';
-
-import 'react-bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css';
+import React from "react";
+import moment from "moment";
+import {FormGroup, ControlLabel} from "react-bootstrap";
+import DateTimeField from "react-bootstrap-datetimepicker";
+import {If} from "react-if";
+import "react-bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css";
 
 const { string, func, array } = React.PropTypes;
 const DATE_FORMAT = 'DD/MM/YYYY HH:mm';
@@ -21,7 +20,7 @@ const FormDateField = (props) => (
       format={DATE_FORMAT}
       inputFormat={DATE_FORMAT}
       onChange={props.onChange} />
-    <If condition={props.errors != undefined}>
+    <If condition={props.errors !== undefined}>
       <div className="text-danger">{props.errors[0]}</div>
     </If>
   </FormGroup>
--- client/app/components/modal/layout/body/FormLocationField.jsx
+++ client/app/components/modal/layout/body/FormLocationField.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
-import { If } from 'react-if';
+import React from "react";
+import {FormGroup, ControlLabel, FormControl} from "react-bootstrap";
+import {If} from "react-if";
 
 const { array, func } = React.PropTypes;
 
