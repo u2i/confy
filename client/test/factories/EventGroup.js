d58006583fd8197fa56ff71694ef861013b503de
Beata Obrok <beata.obrok@u2i.com>
2016-08-08 15:15:54 +0200

    [Fix] event group factory

--- client/test/factories/EventGroup.js
+++ client/test/factories/EventGroup.js
@@ -4,6 +4,6 @@ import Event from './Event';
 
 export default new Factory()
   .option('length', 1)
-  .attr('events', ['length'], length => [...new Array(length).keys()].map(() => Event.build()))
+  .attr('events', ['length'], length => [...new Array(length)].map(() => Event.build()))
   .attr('start', ['events'], events => sortEventsByStartTime(events)[0].start_timestamp)
-  .attr('end', ['events'], events => sortEventsByStartTime[events.length - 1].end_timestamp);
+  .attr('end', ['events'], events => sortEventsByStartTime(events)[events.length - 1].end_timestamp);
