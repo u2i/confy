a1cdf72f312cd553da24a1ddbfe716e7b785022f
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-08-08 11:41:55 +0200

    Updated tests

--- client/test/components/calendar/filters/RoomFilters.spec.jsx
+++ client/test/components/calendar/filters/RoomFilters.spec.jsx
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 import ConferenceRoom from 'test/factories/ConferenceRoom';
 
 describe('<RoomFilters />', () => {
-  const titles = [{ title: 'Zamosc', kind: 0 }, { title: 'Andrychow', kind: 0 }, { title: 'Krakow', kind: 1 }];
+  const titles = [{ title: 'Zamosc', kind: 'narnia' }, { title: 'Andrychow', kind: 'narnia' }, { title: 'Krakow', kind: 'big' }];
   const conferenceRooms = titles.map(({ title, kind }) => ConferenceRoom.build({ title, kind }));
   const props = {
     conferenceRooms,
