c6873e33c14f177d0565105a47e87ea9d4d24677
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-08-22 12:15:40 +0200

    Clearing interval and improved style

--- client/app/components/calendar/TimeIndicatorRow.jsx
+++ client/app/components/calendar/TimeIndicatorRow.jsx
@@ -15,9 +15,13 @@ export default class TimeIndicatorRow extends React.Component {
   };
 
   componentDidMount() {
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.forceUpdate();
     }, 60 * 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
 
   render() {
@@ -32,21 +36,22 @@ export default class TimeIndicatorRow extends React.Component {
 
   _topVal() {
     const minutesFromMidnight = moment().diff(moment().clone().startOf('day'), 'minutes');
-    const top = minutesFromMidnight * parseInt(this.props.rowHeight, 10) / (this.props.unitEventLengthInSeconds / 60);
+    const pixelsPerMinute = parseInt(this.props.rowHeight, 10) / (this.props.unitEventLengthInSeconds / 60);
+    const top = minutesFromMidnight * pixelsPerMinute;
     return `${top}px`;
   }
 
   _timeIndicatorCells() {
     return this.props.days.map(
-      day => {
+      (day, ind) => {
         if (this._isToday(day)) {
           return (
-            <td key={uuid()} className="has-marker">
+            <td key={`IndicatorRow${ind}`} className="has-marker">
               <div id="time-marker" style={{ top: this._topVal() }}></div>
             </td>
           );
         }
-        return <td key={uuid()} />;
+        return <td key={`IndicatorRow${ind}`} />;
       }
     );
   }
