259add1d986c2f9a9efd75297118260f05051e7f
Marcin Henryk Bartkowiak <marcin.bartkowiak@u2i.com>
2016-08-22 12:17:01 +0200

    Removed unusued imports

--- client/app/components/calendar/TimeIndicatorRow.jsx
+++ client/app/components/calendar/TimeIndicatorRow.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import moment from 'moment';
 import instanceOfMoment from 'proptypes/moment';
 import style from './calendar.scss';
-import { v4 as uuid } from 'node-uuid';
 
 export default class TimeIndicatorRow extends React.Component {
   static propTypes = {
