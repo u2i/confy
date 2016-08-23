3e9034dc788258592c40173f8da85cacc5cb81b2
Beata Obrok <beata.obrok@u2i.com>
2016-08-23 15:46:21 +0200

    Setup authorization to access google push notifications

--- app/services/google_oauth.rb
+++ app/services/google_oauth.rb
@@ -8,6 +8,8 @@ class GoogleOauth
   CLIENT_SECRETS = Google::APIClient::ClientSecrets.new(
     YAML.load(ERB.new(File.read(Rails.root.join('config/google_secret.yml'))).result)[Rails.env]
   )
+  CALENDAR_ACCESS_SCOPE = 'https://www.googleapis.com/auth/calendar'
+  USER_INFO_EMAIL_ACCESS_SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
 
   class << self
     def authenticated?(credentials = {})
@@ -29,7 +31,7 @@ class GoogleOauth
     def default_client
       CLIENT_SECRETS.to_authorization.tap do |auth_client|
         auth_client.update!(
-          scope: %w(https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email),
+          scope: [CALENDAR_ACCESS_SCOPE, USER_INFO_EMAIL_ACCESS_SCOPE],
           redirect_uri: (url_for action: :authenticate, controller: :authentication, host: ENV['HOSTNAME'])
         )
       end
@@ -51,6 +53,18 @@ class GoogleOauth
       service.get_userinfo.email
     end
 
+    def push_notification_client
+      Google::Apis::CalendarV3::CalendarService.new.tap { |s| s.authorization = authorization_details }
+    end
+
+    def authorization_details
+      scopes = [CALENDAR_ACCESS_SCOPE]
+      authorization = Google::Auth.get_application_default(scopes)
+      authorization.sub = ENV.fetch('APPLICATION_OWNER')
+      authorization.fetch_access_token!
+      authorization
+    end
+
     def user_info_service(credentials)
       Google::Apis::Oauth2V2::Oauth2Service.new.tap { |s| s.authorization = new_auth_client(credentials) }
     end
@@ -60,5 +74,5 @@ class GoogleOauth
     end
   end
 
-  private_class_method :user_info_service, :new_auth_client
+  private_class_method :user_info_service, :new_auth_client, :authorization_details
 end
