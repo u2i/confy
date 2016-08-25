07beb5da088e7438d86c1dd0547cc02ffc11e473
Beata Obrok <beata.obrok@u2i.com>
2016-08-25 14:25:46 +0200

    Configure action cable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = 'wss://u2i-confy.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://u2i-confy.herokuapp.com', 'http://u2i-confy.herokuapp.com']
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = 'wss://u2i-confy-staging.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://u2i-confy-staging.herokuapp.com', 'http://u2i-confy-staging.herokuapp.com']
 end
