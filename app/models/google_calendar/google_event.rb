e35165a1153ec315c09ef74503fd7f34b438ff80
Beata Obrok <beata.obrok@u2i.com>
2016-08-26 14:53:02 +0200

    Cache event services

--- app/models/google_calendar/google_event.rb
+++ app/models/google_calendar/google_event.rb
@@ -6,22 +6,34 @@ module GoogleCalendar
     end
 
     def create(conference_room_id, raw_event_data = {})
-      GoogleCalendar::EventCreator.new(credentials, user_email).create(conference_room_id, raw_event_data)
+      event_creator.create(conference_room_id, raw_event_data)
     end
 
     def delete(event_id)
-      GoogleCalendar::EventRemover.new(credentials).delete(event_id)
+      event_remover.delete(event_id)
     end
 
     def all(time_interval)
-      GoogleCalendar::EventFinder.new(credentials, user_email).all(time_interval)
+      event_finder.all(time_interval)
     end
 
     def find_by_room(time_interval, conference_room_ids)
-      GoogleCalendar::EventFinder.new(credentials, user_email).by_room(time_interval, conference_room_ids)
+      event_finder.by_room(time_interval, conference_room_ids)
     end
 
     private
+
+    def event_creator
+      @event_creator ||= GoogleCalendar::EventCreator.new(credentials, user_email)
+    end
+
+    def event_remover
+      @event_remover ||= GoogleCalendar::EventRemover.new(credentials)
+    end
+
+    def event_finder
+      @event_finder ||= GoogleCalendar::EventFinder.new(credentials, user_email)
+    end
 
     attr_accessor :credentials, :user_email
   end
