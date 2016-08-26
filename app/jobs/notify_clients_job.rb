727da287c5d282b4cc7ba6130a060850b57730cd
Beata Obrok <beata.obrok@u2i.com>
2016-08-26 14:51:48 +0200

    [Refactor] more meaningful arguments name

--- app/jobs/notify_clients_job.rb
+++ app/jobs/notify_clients_job.rb
@@ -1,8 +1,7 @@
 class NotifyClientsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    conference_room_id = args[0]
+  def perform(conference_room_id)
     ActionCable.server.broadcast('events', conference_room_id: conference_room_id.to_i)
   end
 end
