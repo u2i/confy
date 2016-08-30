7bb81469091cb9ca88b150d25ad20291394f6941
Beata Obrok <beata.obrok@u2i.com>
2016-08-30 14:20:28 +0200

    Pass current and next event as props from rails

--- app/controllers/conference_rooms_controller.rb
+++ app/controllers/conference_rooms_controller.rb
@@ -5,23 +5,29 @@ class ConferenceRoomsController < ApplicationController
   before_action :refresh_token
   before_action :check_authentication
 
-  before_action :fetch_event
-
   def show
-
+    create_props
   end
 
   private
 
-  def fetch_event
-    @event = google_event_client.find_by_room(current_time, [conference_room.id]).first
+  def create_props
+    current_event, next_event = events
+    @props = { current_event: current_event, next_event: next_event, conference_room: conference_room }.compact
+  end
+
+  def events
+    events = google_event_client.find_by_room(current_time, [conference_room.id])
+    id = events.find_index { |e| e[:start][:date_time] <= current_time.starting }
+    return [nil, events.first] unless id
+    [events[id], events[id + 1]]
   end
 
   def current_time
-    TimeInterval.new(Time.now, Time.now + 1.minute).to_rfc3339
+    TimeInterval.new(Time.now, Time.now.end_of_day).to_rfc3339
   end
 
   def conference_room
-    ConferenceRoom.find_by!(title: params[:title].titleize)
+    @conference_room ||= ConferenceRoom.find_by!(title: params[:title].titleize)
   end
 end
