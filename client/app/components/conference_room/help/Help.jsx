7b4503e25bb53bd78cadb0d55f3d975e58d75415
Beata Obrok <beata.obrok@u2i.com>
2016-09-02 14:12:56 +0200

    Modal component for displaying help info

---
+++ client/app/components/conference_room/help/Help.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+import helpText from './help.md';
+
+const Header = () => (
+  <Modal.Header closeButton>
+    <Modal.Title>What is this?</Modal.Title>
+  </Modal.Header>
+);
+
+const Body = () => (
+  <Modal.Body dangerouslySetInnerHTML={{ __html: helpText }} />
+);
+
+const Footer = ({ onHide }) => (
+  <Modal.Footer>
+    <Button onClick={onHide}>Close</Button>
+  </Modal.Footer>
+);
+
+Footer.propTypes = {
+  onHide: React.PropTypes.func
+};
+
+const Help = ({ show, onHide }) => (
+  <Modal show={show} onHide={onHide}>
+    <Header />
+    <Body />
+    <Footer onHide={onHide} />
+  </Modal>
+);
+
+Help.propTypes = {
+  show: React.PropTypes.bool,
+  onHide: React.PropTypes.func
+};
+
+Help.Header = Header;
+Help.Body = Body;
+Help.Footer = Footer;
+
+export default Help;
