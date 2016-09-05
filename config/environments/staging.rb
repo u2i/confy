8fcee7371106372ff6ce62cf14723915bab95b2c
Beata Obrok <beata.obrok@u2i.com>
2016-09-05 12:29:23 +0200

    Set HOSTNAME from HEROKU_APP_NAME if available

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,4 @@
 require Rails.root.join('config/environments/shared_config')
 
 Rails.application.configure do
-  config.web_socket_server_url = 'wss://u2i-confy-staging.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://u2i-confy-staging.herokuapp.com', 'http://u2i-confy-staging.herokuapp.com']
 end
