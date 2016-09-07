704ab0770f04e20bee2e4010686813ef978baf3d
Beata Obrok <beata.obrok@u2i.com>
2016-09-07 11:43:14 +0200

    Split up conference room container component

--- client/app/components/conference_room/ConferenceRoomContainer.jsx
+++ client/app/components/conference_room/ConferenceRoomContainer.jsx
@@ -1,7 +1,6 @@
 import moment from 'moment';
 import get from 'lodash/get';
 import React from 'react';
-import { If, Then, Else } from 'react-if';
 import EventSchema from 'proptypes/schemas/EventSchema';
 import ConferenceRoomSchema from 'proptypes/schemas/ConferenceRoomSchema';
 import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
@@ -15,6 +14,36 @@ import './conference_room.scss';
 
 const NO_MORE_EVENTS_TEXT = 'No more events for today';
 
+const CurrentAndNextEvents = ({ currentEvent, nextEvents, onUpdate }) => (
+  <Row>
+    <Col xs={12} sm={8}>
+      <CurrentEvent event={currentEvent}
+                    nextEventStart={moment(get(nextEvents[0], 'start.date_time'))}
+                    onCompleted={onUpdate} />
+    </Col>
+    <Col xs={12} sm={4}>
+      <NextEvents events={nextEvents} noEventLabel={NO_MORE_EVENTS_TEXT} />
+    </Col>
+  </Row>
+);
+
+CurrentAndNextEvents.propTypes = {
+  currentEvent: EventSchema.except('width', 'offset'),
+  nextEvents: React.PropTypes.arrayOf(EventSchema.only('start')),
+  onUpdate: React.PropTypes.func
+};
+
+const NoEvents = () => (
+  <Row>
+    <Col xs={12}>
+      <Jumbotron>
+        <h3>{NO_MORE_EVENTS_TEXT}</h3>
+        <p>Check back tomorrow!</p>
+      </Jumbotron>
+    </Col>
+  </Row>
+);
+
 const ConferenceRoomContainer = ({
   currentEvent,
   nextEvents,
@@ -24,28 +53,10 @@ const ConferenceRoomContainer = ({
   <div>
     <Navbar conferenceRoom={conferenceRoom} />
     <Grid className="conference-room-container">
-      <If condition={!!currentEvent || nextEvents.length > 0}>
-        <Then>
-          <Row>
-            <Col xs={12} sm={8}>
-              <CurrentEvent event={currentEvent}
-                            nextEventStart={moment(get(nextEvents[0], 'start.date_time'))}
-                            onCompleted={onUpdate} />
-            </Col>
-            <Col xs={12} sm={4}>
-              <NextEvents events={nextEvents} noEventLabel={NO_MORE_EVENTS_TEXT} />
-            </Col>
-          </Row>
-        </Then>
-        <Else>
-          <Col xs={12}>
-            <Jumbotron>
-              <h3>{NO_MORE_EVENTS_TEXT}</h3>
-              <p>Check back tomorrow!</p>
-            </Jumbotron>
-          </Col>
-        </Else>
-      </If>
+      {currentEvent || nextEvents.length > 0 ?
+        <CurrentAndNextEvents currentEvent={currentEvent} nextEvents={nextEvents} onUpdate={onUpdate} /> :
+        <NoEvents />
+      }
     </Grid>
   </div>
 );
