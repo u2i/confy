ffa7033c224b251866c7257662f57e465a2233ec
Beata Obrok <beata.obrok@u2i.com>
2016-09-08 16:06:41 +0200

    [Tests] for tablet view buttons

---
+++ client/test/components/conference_room/layout/controls/Controls.spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Event from 'test/factories/event';
+
+import Controls from 'components/conference_room/layout/controls/Controls';
+import EventControls from 'components/conference_room/layout/controls/event/EventControls';
+import NoEventControls from 'components/conference_room/layout/controls/NoEventControls';
+
+describe('<Controls />', () => {
+  context('with event', () => {
+    const event = Event.build();
+    const wrapper = shallow(<Controls event={event} />);
+
+    it('renders <EventControls />', () => {
+      expect(wrapper).to.have.exactly(1).descendants(EventControls);
+    });
+  });
+
+  context('with no event', () => {
+    const wrapper = shallow(<Controls />);
+
+    it('renders <NoEventControls />', () => {
+      expect(wrapper).to.have.exactly(1).descendants(NoEventControls);
+    });
+  });
+});
---
+++ client/test/components/conference_room/layout/controls/event/EventControls.spec.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Event from 'test/factories/event';
+
+import EventControls from 'components/conference_room/layout/controls/event/EventControls';
+import InProgress from 'components/conference_room/layout/controls/event/InProgress';
+import Unstarted from 'components/conference_room/layout/controls/event/Unstarted';
+
+describe('<EventControls />', () => {
+  context('with unconfirmed event', () => {
+    const event = Event.build();
+    const wrapper = shallow(<EventControls event={event} />);
+
+    it('renders <Unstarted />', () => {
+      expect(wrapper).to.have.exactly(1).descendants(Unstarted);
+    });
+  });
+
+  context('with confirmed event', () => {
+    const event = Event.build({ confirmed: true });
+    const wrapper = shallow(<EventControls event={event} />);
+
+    it('renders <InProgress />', () => {
+      expect(wrapper).to.have.exactly(1).descendants(InProgress);
+    });
+  });
+});
