3a2708fda9a092a11e3bb572b441a4fc1f04f19b
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-09-08 09:28:10 +0200

    Refresh token fix

--- app/controllers/concerns/google_authentication.rb
+++ app/controllers/concerns/google_authentication.rb
@@ -3,22 +3,45 @@ module GoogleAuthentication
 
   included do
     rescue_from ArgumentError do
-      session.delete(:credentials)
+      clear_credentials
       redirect_action
     end
   end
 
   def refresh_token
-    credentials = session[:credentials]
-    if credentials && GoogleOauth.need_to_refresh_token?(credentials)
-      session[:credentials] = GoogleOauth.refresh_token(credentials)
-      session[:email] = GoogleOauth.user_email(session[:credentials])
+    clear_credentials unless refresh_token_present?
+    if user_credentials && GoogleOauth.need_to_refresh_token?(user_credentials)
+      update_credentials
+      update_user_email
     end
+  rescue Signet::AuthorizationError
+    clear_credentials
   end
 
+  def clear_credentials
+    session.delete(:credentials)
+  end
+
+  def refresh_token_present?
+    user_credentials && JSON.parse(user_credentials)['refresh_token'].present?
+  end
+
+  def user_credentials
+    session[:credentials]
+  end
+
+  def update_credentials
+    session[:credentials] = GoogleOauth.refresh_token(user_credentials)
+  end
+
+  def update_user_email
+    session[:email] = GoogleOauth.user_email(user_credentials)
+  end
+
+
   def check_authentication
-    return redirect_action unless session[:credentials]
-    redirect_action unless GoogleOauth.authenticated?(JSON.parse(session[:credentials]))
+    return redirect_action unless user_credentials
+    redirect_action unless GoogleOauth.authenticated?(JSON.parse(user_credentials))
   end
 
   private
