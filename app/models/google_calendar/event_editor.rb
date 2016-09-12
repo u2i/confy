b652f139d215a09eedf5a959185ffe5466d1a35a
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-09-12 12:00:06 +0200

    replaced instance variables in event_editor with method arguments

--- app/models/google_calendar/event_editor.rb
+++ app/models/google_calendar/event_editor.rb
@@ -6,39 +6,39 @@ module GoogleCalendar
     end
 
     def finish(conference_room_id, event_id)
-      @conference_room_email = ConferenceRoom.find(conference_room_id).email
-      @event = get_event(event_id)
-      finish_event
+      conference_room_email = ConferenceRoom.find(conference_room_id).email
+      event = get_event(event_id, conference_room_email)
+      finish_event(event, conference_room_email)
     end
 
     private
 
     attr_accessor :event_id, :calendar_service, :conference_room_email, :event
 
-    def get_event(event_id)
+    def get_event(event_id, conference_room_email)
       calendar_service.get_event(conference_room_email, event_id)
     end
 
-    def update_event
-      calendar_service.update_event(conference_room_email, event.id, event)
-    end
-
-    def finish_event
-      return if event_not_started? || event_finished?
+    def finish_event(event, conference_room_email)
+      return if event_not_started?(event) || event_finished?(event)
       event.end.date_time = current_time
-      update_event
+      update_event(event, conference_room_email)
     end
 
-    def event_not_started?
+    def event_not_started?(event)
       event.start.date_time > current_time
     end
 
-    def event_finished?
+    def event_finished?(event)
       event.end.date_time < current_time
     end
 
     def current_time
       DateTime.now
+    end
+
+    def update_event(event, conference_room_email)
+      calendar_service.update_event(conference_room_email, event.id, event)
     end
   end
 end
