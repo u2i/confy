b3b5030076f2149aa60bd49be3e61509b7dd09db
Lukasz Kudyba <lkudyba@hotmail.com>
2016-09-14 15:30:13 +0200

    Move Link to Hangout to Additional Details

--- client/app/components/calendar/event/details/EventAdditionalDetails.jsx
+++ client/app/components/calendar/event/details/EventAdditionalDetails.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import EventAttendees from './EventAttendees';
 import EventDescription from './EventDescription';
+import EventHangoutLink from './EventHangoutLink';
 import EventSchema from 'proptypes/schemas/EventSchema';
 
 const EventAdditionalDetails = ({ event }) => (
   <div>
     <EventDescription event={event} />
     <EventAttendees event={event} />
+    <EventHangoutLink event={event} />
   </div>
 );
 
