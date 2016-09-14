b6c0532a99ff5fd10aac992c7211f3bc57c08f2c
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-09-14 17:05:49 +0200

    Added expanding event description feature

--- client/app/components/calendar/event/details/EventDescription.jsx
+++ client/app/components/calendar/event/details/EventDescription.jsx
@@ -2,19 +2,63 @@ import React from 'react';
 import { If } from 'react-if';
 import EventSchema from 'proptypes/schemas/EventSchema';
 
-const EventDescription = ({ event }) => (
-  <div className="event-description">
-    <If condition={typeof event.description !== 'undefined'}>
-      <div>
-        <small>Description:&nbsp;</small>
-        <p>{event.description}</p>
+const DESCRIPTION_LENGTH = 50;
+export default class EventDescription extends React.Component {
+  static propTypes = {
+    event: EventSchema.only('description').isRequired
+  };
+
+  constructor(...args) {
+    super(...args);
+    this.state = { fullDescription: false };
+
+    this._handleFullDescription = this._handleFullDescription.bind(this);
+  }
+
+  _descriptionContent() {
+    if (!this.props.event.description) {
+      return;
+    }
+    else if (this.state.fullDescription) {
+      return this._fullDescription();
+    }
+    return this._shortDescription();
+  }
+
+  _shortDescription() {
+    return this.props.event.description.slice(0,DESCRIPTION_LENGTH) + '...';
+  }
+
+  _fullDescription() {
+    return this.props.event.description;
+  }
+
+  _changeDescriptionLength() {
+    this.setState({ fullDescription: !this.state.fullDescription });
+  }
+
+  _handleFullDescription(event) {
+    event.stopPropagation();
+    this._changeDescriptionLength();
+  }
+
+  _expansionMessage() {
+    return this.state.fullDescription ? ' Less' : ' More';
+  }
+
+  render() {
+    return (
+      <div className="event-description">
+        <If condition={typeof this.props.event.description !== 'undefined'}>
+          <div>
+            <small>Description:&nbsp;</small>
+            <p style={{display: 'inline'}}>{this._descriptionContent()}</p>
+            <div style={{display: 'inline'}} onClick={this._handleFullDescription}>
+              <a>{this._expansionMessage()}</a>
+            </div>
+          </div>
+        </If>
       </div>
-    </If>
-  </div>
-);
-
-EventDescription.propTypes = {
-  event: EventSchema.only('description').isRequired
+    );
+  }
 };
-
-export default EventDescription;
