a733088fc764da894e1651be86181a8e7e73ec96
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-09-19 14:59:51 +0200

    refactoring

--- client/app/components/calendar/modal/layout/body/RecurrenceComponent.jsx
+++ client/app/components/calendar/modal/layout/body/RecurrenceComponent.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
-import { FormGroup, ControlLabel, FormControl, Radio } from 'react-bootstrap';
+import { FormGroup, ControlLabel, Radio } from 'react-bootstrap';
 
 const RECURRENCE_OPTIONS = ['none', 'daily', 'weekly', 'every other week', 'monthly'];
 
 export default class RecurrenceComponent extends React.Component {
+  static propTypes = {
+    onChange: React.PropTypes.func.isRequired
+  };
+
   constructor(...args) {
     super(...args);
 
     this.state = { checked: 'none' };
     this._isChecked = this._isChecked.bind(this);
-  };
-
-  _onChange(option, e) {
-    e.stopPropagation();
-    this.setState({ checked: option});
-    this.props.onChange(option);
-  };
-
-  _isChecked(option) {
-    return option === this.state.checked;
-  };
-
-  _recurrenceOptions() {
-    return RECURRENCE_OPTIONS.map(option => (
-      <Radio key={option} onChange={this._onChange.bind(this, option)} checked={this._isChecked(option)} inline>
-        {option}
-      </Radio>
-    ));
+    this._onChange = this._onChange.bind(this);
   }
 
   render() {
@@ -36,5 +23,23 @@ export default class RecurrenceComponent extends React.Component {
         {this._recurrenceOptions()}
       </FormGroup>
     );
+  }
+
+  _recurrenceOptions() {
+    return RECURRENCE_OPTIONS.map(option => (
+      <Radio key={option} value={option} onChange={this._onChange} checked={this._isChecked(option)} inline>
+        {option}
+      </Radio>
+    ));
+  }
+
+  _onChange(e) {
+    const option = e.target.value;
+    this.setState({ checked: option });
+    this.props.onChange(option);
+  }
+
+  _isChecked(option) {
+    return option === this.state.checked;
   }
 }
