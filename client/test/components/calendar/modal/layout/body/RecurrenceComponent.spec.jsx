76809fcfef03a216929eb94833baf8ca8f34da3e
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-09-21 09:49:48 +0200

    replaced mount wrappers with shallow wrappers for some tests in RecurrenceComponent

--- client/test/components/calendar/modal/layout/body/RecurrenceComponent.spec.jsx
+++ client/test/components/calendar/modal/layout/body/RecurrenceComponent.spec.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Radio } from 'react-bootstrap';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import RecurrenceComponent from 'components/calendar/modal/layout/body/RecurrenceComponent';
@@ -8,27 +7,28 @@ import sinon from 'sinon';
 describe('<RecurrenceComponent />', () => {
   const onChange = sinon.spy();
   const mountRecurrenceWrapper = () => mount(<RecurrenceComponent onChange={onChange} />);
+  const shallowRecurrenceWrapper = () => shallow(<RecurrenceComponent onChange={onChange} />);
   let wrapper;
 
   it('renders <Radio /> for each recurrence option', () => {
-    wrapper = mountRecurrenceWrapper();
+    wrapper = shallowRecurrenceWrapper();
     expect(wrapper.find('Radio')).to.have.lengthOf(5);
   });
 
   it('renders <Radio />  with "none" value initially checked', () => {
-    wrapper = mountRecurrenceWrapper();
+    wrapper = shallowRecurrenceWrapper();
     const noneRadio = wrapper.find('Radio').filterWhere(node => node.prop('value') === 'none');
 
     expect(noneRadio.props().checked).to.eq(true);
   });
 
   it('renders remaining <Radio /> initially unchecked', () => {
-    wrapper = mountRecurrenceWrapper();
+    wrapper = shallowRecurrenceWrapper();
     const remainingRadios = wrapper.find('Radio').filterWhere(node => node.prop('value') !== 'none');
 
     remainingRadios.forEach(radio => {
       expect(radio.props().checked).to.eq(false);
-    })
+    });
   });
 
   context('invoked change event on <Radio />', () => {
