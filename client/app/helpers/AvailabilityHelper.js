05993450eb3b51e07e42d8de20622ba2491bfd12
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2016-09-27 14:25:49 +0200

    Code style

--- client/app/helpers/AvailabilityHelper.js
+++ client/app/helpers/AvailabilityHelper.js
@@ -1,4 +1,4 @@
-import { currentAndNextEvents } from 'helpers/EventHelper';
+import { currentAndNextEvents } from './EventHelper';
 import { durationFromNow } from 'helpers/DateHelper';
 import sortBy from 'lodash/sortBy';
 import moment from 'moment';
@@ -8,16 +8,6 @@ export const AVAILABILITY = {
   CURRENTLY_AVAILABLE: 1,
   BUSY: 2
 };
-
-export function roomAvailabilityProps(conferenceRoom, allEvents) {
-  const events = eventsInConferenceRoom(allEvents, conferenceRoom.id);
-  if(events.length === 0) return noEventsTodayProps(conferenceRoom);
-
-  const { current, next } = currentAndNextEvents(events);
-  if(current === undefined) return timeTillNextEventProps(conferenceRoom, next[0]);
-
-  return occupiedTimeProps(conferenceRoom, events);
-}
 
 function eventsInConferenceRoom(events, conferenceRoomId) {
   return events.filter(event => event.conference_room.id === conferenceRoomId);
@@ -40,34 +30,40 @@ function occupiedTimeProps(conferenceRoom, events) {
 }
 
 function lastEventEndTime(events) {
-  for (var i = 0; i < events.length - 1; i++) {
-    if (moment(events[i+1].start.date_time).diff(moment(events[i].end.date_time)) >= 1000 * 60) {
+  for (let i = 0; i < events.length - 1; i++) {
+    if (moment(events[i + 1].start.date_time).diff(moment(events[i].end.date_time)) >= 1000 * 60) {
       return moment(events[i].end.date_time);
     }
   }
   return moment(events[events.length - 1].end.date_time);
 }
 
-export function sortByAvailability(props) {
-  props.sort((a, b) => {
-    return compareByAvailability(a, b);
-  });
+export function roomAvailabilityProps(conferenceRoom, allEvents) {
+  const events = eventsInConferenceRoom(allEvents, conferenceRoom.id);
+  if (events.length === 0) return noEventsTodayProps(conferenceRoom);
+
+  const { current, next } = currentAndNextEvents(events);
+  if (current === undefined) return timeTillNextEventProps(conferenceRoom, next[0]);
+
+  return occupiedTimeProps(conferenceRoom, events);
 }
 
-function compareByAvailability(a, b) {
-  if (a.availability === b.availability) {
-    if (a.availability === AVAILABILITY.CURRENTLY_AVAILABLE) {
-      return a.duration <= b.duration;
+function compareByAvailability(left, right) {
+  if (left.availability === right.availability) {
+    if (left.availability === AVAILABILITY.CURRENTLY_AVAILABLE) {
+      return left.duration <= right.duration;
     }
-    if (a.availability === AVAILABILITY.BUSY) {
-      return a.duration >= b.duration;
+    if (left.availability === AVAILABILITY.BUSY) {
+      return left.duration >= right.duration;
     }
   }
-  return a.availability >= b.availability;
+  return left.availability >= right.availability;
+}
+
+export function sortByAvailability(props) {
+  props.sort((left, right) => compareByAvailability(left, right));
 }
 
 export function buildAvailabilityProps(conferenceRooms, events) {
-  return conferenceRooms.map(conferenceRoom => {
-    return roomAvailabilityProps(conferenceRoom, events);
-  });
+  return conferenceRooms.map(conferenceRoom => roomAvailabilityProps(conferenceRoom, events));
 }
