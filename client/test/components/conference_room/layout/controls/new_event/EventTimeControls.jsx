4b94130f6e64bb73672e90d315c7419ef2a437ca
Beata Obrok <beata.obrok@u2i.com>
2016-09-27 14:51:38 +0200

    [Fix] EventTimeControls tests

--- client/test/components/conference_room/layout/controls/new_event/EventTimeControls.jsx
+++ client/test/components/conference_room/layout/controls/new_event/EventTimeControls.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import moment from 'moment';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
-import EventTimeControls from 'components/conference_room/layout/controls/new_event/EventTimeControls';
+import eventTimeControls from 'components/conference_room/layout/controls/new_event/EventTimeControls';
 import TimeChoiceButton from 'components/conference_room/layout/controls/new_event/TimeChoiceButton';
 import AvailableTimeButton from 'components/conference_room/layout/controls/new_event/AvailableTimeButton';
 
 describe('<EventTimeControls />', () => {
+  const ControlsContainer = (props) => <div>{eventTimeControls(props)}</div>;
+
   it('renders time buttons', () => {
-    const wrapper = shallow(<EventTimeControls />);
+    const wrapper = shallow(<ControlsContainer />);
     expect(wrapper.find(TimeChoiceButton)).to.have.lengthOf(3);
   });
 
   context('without next event', () => {
-    const wrapper = mount(<EventTimeControls />);
+    const wrapper = mount(<ControlsContainer />);
 
     it('does not render <AvailableTimeButton />', () => {
       expect(wrapper).not.to.have.descendants(AvailableTimeButton);
@@ -21,7 +23,7 @@ describe('<EventTimeControls />', () => {
   });
 
   context('with next event', () => {
-    const wrapper = mount(<EventTimeControls nextEventStart={moment().add(2, 'hours')} />);
+    const wrapper = mount(<ControlsContainer nextEventStart={moment().add(2, 'hours')} />);
 
     it('does not render <AvailableTimeButton />', () => {
       expect(wrapper).to.have.descendants(AvailableTimeButton);
