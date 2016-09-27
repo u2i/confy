7480e1179f908774520c1d3aaac76e91a4705bce
Beata Obrok <beata.obrok@u2i.com>
2016-09-27 17:36:57 +0200

    [Fix] NoEventControls tests

--- client/test/components/conference_room/layout/controls/NoEventControls.spec.jsx
+++ client/test/components/conference_room/layout/controls/NoEventControls.spec.jsx
@@ -1,45 +1,16 @@
 import React from 'react';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import NoEventControls from 'components/conference_room/layout/controls/NoEventControls';
-import StartButton from 'components/conference_room/layout/controls/StartButton';
-import EventTimeControls from 'components/conference_room/layout/controls/new_event/EventTimeControls';
+import CreateEventControls from 'components/conference_room/layout/controls/new_event/CreateEventControls';
 
 describe('<NoEventControls />', () => {
-  it('renders <StartButton /> by default', () => {
-    const wrapper = mount(<NoEventControls />);
-    expect(wrapper).to.have.exactly(1).descendants(StartButton);
-    expect(wrapper).not.to.have.descendants(EventTimeControls);
-  });
+  const wrapper = shallow(<NoEventControls />);
+  const controls = wrapper.find(CreateEventControls);
 
-  context('when <StartButton /> is clicked', () => {
-    const wrapper = mount(<NoEventControls />);
-
-    before(() => {
-      wrapper.find(StartButton).find('button').simulate('click');
-    });
-
-    it('renders <EventTimeControls />', () => {
-      expect(wrapper).to.have.exactly(1).descendants(EventTimeControls);
-      expect(wrapper).not.to.have.descendants(StartButton);
-    });
-  });
-
-  context('when cancel button is clicked', () => {
-    const wrapper = mount(<NoEventControls />);
-
-    before(() => {
-      wrapper.find(StartButton).find('button').simulate('click');
-      wrapper.find(EventTimeControls)
-        .find('button')
-        .findWhere(button => button.text() === 'Cancel')
-        .simulate('click');
-    });
-
-    it('renders <StartButton />', () => {
-      expect(wrapper).to.have.exactly(1).descendants(StartButton);
-      expect(wrapper).not.to.have.descendants(EventTimeControls);
-    });
+  it('renders <CreateEventControls /> with "Start" toggle', () => {
+    expect(controls).to.have.lengthOf(1);
+    expect(controls).to.have.prop('toggleText').equal('Start');
   });
 });
