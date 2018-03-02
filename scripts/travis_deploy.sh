e037d8309bf273f54082939c132c77dc5f299ce6
Piotr Jatkowski <piotr.jatkowski@u2i.com>
2018-03-02 10:09:47 +0100

    Make travis_deploy script executable.

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -2,6 +2,6 @@
 
 if [ -n "$TRAVIS_TAG" ]; then
   gcloud auth activate-service-account --key-file gcloud_credentials.json
-  docker tag confy:"$CONFY_VERSION" gcr.io/kubernetes-playground-195112/confy:"$CONFY_VERSION"
-  gcloud docker -- push gcr.io/kubernetes-playground-195112/confy:"$CONFY_VERSION"
+  docker tag confy:"$TRAVIS_TAG" gcr.io/kubernetes-playground-195112/confy:"$TRAVIS_TAG"
+  gcloud docker -- push gcr.io/kubernetes-playground-195112/confy:"$TRAVIS_TAG"
 fi
