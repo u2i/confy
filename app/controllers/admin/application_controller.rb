c2e4fddabdf72beb87f04d6a08355987356d4f15
Tomasz Godniak <tomasz.godniak@u2i.com>
2018-11-16 16:47:29 +0100

    Update password for admin section

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -5,8 +5,14 @@ module Admin
     before_action :basic_authentication
 
     def basic_authentication
+      credentials = {
+        test: { username: 'confy-admin', password: '47dc842071d09a6fed87056ff4b4fd5a' },
+        prod: { username: 'confy-admin', password: 'bff2404709f2d110ca9da4cfe6fd7d55' }
+      }
+      env = Rails.env.test? ? :test : :prod
       authenticate_or_request_with_http_basic('Confy Admin Panel') do |username, password|
-        username == 'confy-admin' && password == 'confy-pass'
+        md5_of_password = Digest::MD5.hexdigest(password)
+        username == credentials[env][:username] && md5_of_password == credentials[env][:password]
       end
     end
   end
