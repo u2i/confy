8cf7429aed5bb75bd40cf2c06f9a00bd56a034ad
Tomasz Godniak <tomasz.godniak@u2i.com>
2018-11-19 11:31:22 +0100

    Add no jQuery confirm and delete method support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,15 @@ module ApplicationHelper
   rescue
     TimeInterval.week(date_param)
   end
+
+  def delete_button_to(title, url, options = {})
+    html_options = {
+      class: 'delete_button_to',
+      method: :delete
+    }.merge(options.delete(:html_options) || {})
+
+    form_for :delete, url: url, html: html_options do |f|
+      f.submit title, options
+    end
+  end
 end
