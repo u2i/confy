25cb3e2be2f6e9f152231458949d72b3ff4c2f7c
Tomasz Godniak <tomasz.godniak@u2i.com>
2018-11-20 10:39:08 +0100

    Update specs and version in app.json

--- spec/requests/api/events_spec.rb
+++ spec/requests/api/events_spec.rb
@@ -7,15 +7,8 @@ RSpec.describe 'Api::Events', type: :request do
     let(:calendar_service) { double }
     let(:service_account_client) { double }
     let(:email) { ENV.fetch('APPLICATION_OWNER') }
-    let(:conference_room) { create :conference_room }
-    let(:with_confirmation) { true }
 
-    subject do
-      get api_events_path, headers: headers, params: {
-        conference_room_id: conference_room.id,
-        confirmation: with_confirmation
-      }
-    end
+    subject { get api_events_path, headers: headers }
 
     context 'when events are returned' do
       before do
@@ -24,7 +17,7 @@ RSpec.describe 'Api::Events', type: :request do
         expect(GoogleCalendar::GoogleEvent)
           .to receive(:new).with(calendar_service: calendar_service, user_email: email) { service_account_client }
         expect(service_account_client)
-          .to receive(:find_by_room).with(TimeInterval::TimeIntervalRFC3339, conference_room.id, with_confirmation) { [] }
+          .to receive(:all).with(TimeInterval::TimeIntervalRFC3339) { [] }
       end
 
       it_behaves_like 'successfull request'
