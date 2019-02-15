950c579037ab457ddbf2038d5d4c714695e48989
Tomasz Godniak <tomasz.godniak@u2i.com>
2019-02-15 10:55:18 +0100

    Improve attendeeName method

--- native/helpers/AttendeeHelper.js
+++ native/helpers/AttendeeHelper.js
@@ -5,10 +5,14 @@ export const attendeeName = (guest) => {
   if (guest.display_name) {
     return guest.display_name;
   }
+  else if (guest.email.match('@u2i.com')) {
+    const name = guest.email.replace(/@.*$/, '');
 
-  const name = guest.email.replace(/@.*$/, '');
-
-  return map(name.split('.'), upperFirst).join(' ');
+    return map(name.split('.'), upperFirst).join(' ');
+  }
+  else {
+    return guest.email;
+  }
 }
 
 export const attendeeClass = (guest) => {
